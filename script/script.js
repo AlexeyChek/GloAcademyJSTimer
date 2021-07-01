5e72883a1188ace5c74387b469fd8882f715fd5d
AlexeyChekalin <sphinxsk@yandex.ru>
2021-07-01 23:31:55 +0300

    fixeddddddddd smooth

--- script/script.js
+++ script/script.js
@@ -71,10 +71,10 @@ toggleMenu();
 // smoothScroll
 const smoothScroll = () => {
   document.addEventListener('click', event => {
-    event.preventDefault();
     const target = event.target.closest('a[href^="#"');
     if (target) {
       if (!target.classList.contains('close-btn') && !target.classList.contains('portfolio-btn')) {
+        event.preventDefault();
         const href = target.getAttribute('href').substring(1);
         const position = document.getElementById(href).getBoundingClientRect().top;
         window.scrollBy({
